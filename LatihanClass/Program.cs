5bcd6138497e2d3f561847e63c83f278ba77dfd3
adit <aditya@gmail.com>
2019-04-09 11:58:32 +0700

    commit pertama

---
+++ LatihanClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time ObjTime = new Time(2000, 05, 13, 10, 10, 10);
+            Hasil(ObjTime);
+            Console.ReadKey();
+
+        }
+
+        static void Hasil(Time ObjTime)
+        {
+            Console.WriteLine("Year : {0}", ObjTime.Year.ToString());
+            Console.WriteLine("Month : {0}", ObjTime.Month.ToString());
+            Console.WriteLine("Date : {0}", ObjTime.Date);
+            Console.WriteLine("Hour : {0}", ObjTime.Hour);
+            Console.WriteLine("Minute : {0}", ObjTime.Minute);
+            Console.WriteLine("Second : {0}", ObjTime.Second);
+        }
+    }
+}
---
+++ LatihanClass/Time.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Time
+    {
+        // private variables atau field
+        private int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+        // public methods
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Date { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public Time()
+        {
+            Year = 2018;
+            Month = 10;
+            Date = 10;
+            Hour = 10;
+            Minute = 10;
+            Second = 50;
+        }
+
+        public Time(int year, int month, int date, int hour, int minute, int second)
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Date = date;
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = second;        }
+    }
+}
